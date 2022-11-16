57f0b7cfba506d800dd4ce61366e76de610c53d8
Opilane TTHK <opilane@tthk.ee>
2022-11-16 11:21:07 +0200

    Update Program.cs

--- 16.11/ConsoleApp1/ConsoleApp1/Program.cs
+++ 16.11/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,26 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a number");
+            string number = Console.ReadLine();
+            int numberParsed = Int16.Parse(number);
+
+            if (numberParsed < 1)
+            {
+                Console.WriteLine("See literally ei eksisteeri");
+            }
+            else if (numberParsed > 99)
+            {
+                Console.WriteLine("See ei eksisteeri");
+            }
+            if (numberParsed == 7)
+            {
+                Console.WriteLine("Ma ei tea jalkast palju aga sen Ronaldo");
+            }
+            if (numberParsed == 30)
+            {
+                Console.WriteLine("Sen Messi. ära messi temaga");
+            }
         }
     }
 }
